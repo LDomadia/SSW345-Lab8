b0b4ed85a0196e6a386c5f449ab9c10e9bafafc3
LDomadia <leena.domadia2018@gmail.com>
2021-03-25 14:03:28 -0400

    Lab part 4 complete

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // Require packages and set the port
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3002;
 const app = express();
 
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
 app.get('/', (request, response) => {
     console.log(`URL: ${request.url}`);
-    response.send('Hello, Server!');
+    response.send({message: 'Node.js and Express REST API'});;
 });
 
 // Start the server
