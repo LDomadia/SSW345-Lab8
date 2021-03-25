f9bcd5af22a42e694459a51454184452a22317bf
LDomadia <leena.domadia2018@gmail.com>
2021-03-25 14:18:45 -0400

    Lab part 5 complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Require packages and set the port
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes/routes.js');
 const port = 3002;
 const app = express();
 
@@ -10,10 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-app.get('/', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send({message: 'Node.js and Express REST API'});;
-});
+routes(app);
 
 // Start the server
 const server = app.listen(port, (error) => {
