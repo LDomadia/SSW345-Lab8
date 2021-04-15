697b2a68312692e4a8b959ef8c7a06e8e5fc6c01
Mathieu Nagle <62767413+Mathieu-Nagle@users.noreply.github.com>
2021-04-15 14:22:28 -0400

    init eslint and format app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 // Require packages and set the port
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes/routes.js');
-const port = 3002;
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes/routes.js')
+const port = 3002
+const app = express()
 
 // Use Node.js body parsing middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended: true,
-}));
+  extended: true
+}))
 
-routes(app);
+routes(app)
 
 // Start the server
 const server = app.listen(port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
- 
-    console.log(`Server listening on port ${server.address().port}`);
-});
+  if (error) return console.log(`Error: ${error}`)
+  console.log(`Server listening on port ${server.address().port}`)
+})
